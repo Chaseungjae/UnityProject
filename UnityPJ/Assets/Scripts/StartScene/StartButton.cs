4ec2537d11f134a8f3e4c1780562bf48fda22e88
dhehgur0125 <dhehgur0125@naver.com>
2025-11-20 22:11:42 +0900

    시작화면 대충 구현과 붉은 하늘 고침

---
+++ UnityPJ/Assets/Scripts/StartScene/StartButton.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class StartButton : MonoBehaviour
+{
+    public SceneAsset sceneLoader;  // Inspector에서 이동할 씬 드롭
+    private string sceneName;
+    void Awake()
+    {
+#if UNITY_EDITOR
+        if (sceneLoader != null)
+        {
+            string path = AssetDatabase.GetAssetPath(sceneLoader);
+            sceneName = System.IO.Path.GetFileNameWithoutExtension(path);
+        }
+#endif
+    }
+
+    public void OnButtonClick()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
